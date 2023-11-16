7c179fabb0861f8130f09fb590e79360da5b8df9
Guillermo Hedzko <ghedzko@gmail.com>
2023-11-16 11:55:04 -0300

    Add RabbitMQModule to producer module

--- apps/producer/src/producer.module.ts
+++ apps/producer/src/producer.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ProducerController } from './producer.controller';
 import { ProducerService } from './producer.service';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 @Module({
-  imports: [],
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'producer',
+          type: 'fanout',
+        },
+      ],
+      uri: 'amqp://rabbitmq:5672',
+      channels: {
+        'channel-1': {
+          prefetchCount: 15,
+          default: true,
+        },
+        'channel-2': {
+          prefetchCount: 2,
+        },
+      },
+    }),
+  ],
   controllers: [ProducerController],
   providers: [ProducerService],
 })
